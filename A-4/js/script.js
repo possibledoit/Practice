55acaa66bf2a1c7061c36e329409e1d903594902
possibledoit <156152383+possibledoit@users.noreply.github.com>
2024-06-28 13:57:17 +0900

    독학

---
+++ A-4/js/script.js
@@ -0,0 +1,47 @@
+$(function() {
+
+    // 슬라이드
+    let i = 0
+
+    function slide() {
+        if(i < 2){
+            i++;
+        }
+        else {
+            i = 0;
+        }
+        $(".slide ul li").fadeOut()
+        $(".slide ul li").eq(i).fadeIn()
+    }
+    setInterval(slide, 2000)
+
+    // 메뉴 키보드이벤트
+    $("nav>ul>li").focusin(function() {
+        $(this).addClass("on")
+        $(".sub, .bg").slideDown()
+    })
+    $("nav li li:last-child").focusout(function() {
+        $("nav>ul>li").removeClass("on")
+    })
+    $("nav li:last-child li:last-child").focusout(function() {
+        $(".sub, .bg").slideUp()
+    })
+
+    // 메뉴
+    $("nav>ul>li").mouseenter(function() {
+        $(".sub, .bg").stop().slideDown()
+    })
+    $("nav>ul>li").mouseleave(function() {
+        $(".sub, .bg").stop().slideUp()
+    })
+
+    // 팝업창
+    $(".pp").click(function() {
+        $(".popup").show();
+        return false;
+    })
+
+    $(".close").click(function() {
+        $(".popup").hide()
+    })
+})
