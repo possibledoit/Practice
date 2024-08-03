f6c84abff1c3dcd515a5d8f1ea7b022105ce6290
possibledoit <156152383+possibledoit@users.noreply.github.com>
2024-08-04 00:19:31 +0900

    독학

--- script.js
+++ script.js
@@ -1,38 +1,37 @@
 $(function() {
 
-    // 슬라이드
+    // 위에서 아래 이동 슬라이드
+    const slides = $('.slide_container>ul');
     function slide() {
-        $(".slide ul li").animate({left:"-1200px"},1000,function() {
-            $(".slide ul").append($(".slide ul li").first())
-            $(".slide ul").css({left:0})
+        slides.animate({'top': '-300px'}, 450, function() {
+            slides.find('li').eq(0).appendTo(slides);
+            slides.css('top', 0);
         })
     }
-    setInterval(slide, 3000)
+    setInterval(slide, 3000);
 
+    // 좌에서 우로 이동 슬라이드
+    const slides = $('.slide_container>ul');
     function slide() {
-        $(".slide ul li").animate({top:"-300px"},1000,function() {
-            $(".slide ul").append($(".slide ul li").first())
-            $(".siide ul").css({top:0})
+        slides.animate({'left': '-1200px'}, 450, function() {
+            slides.find('li').eq(0).appendTo(slides);
+            slides.css('left', 0);
         })
     }
-    setInterval(slide, 3000)
+    setInterval(slide, 3000);
 
-    function slide() {
-        if(i<2) {
-            i++
-        }
-        else {
-            i=0
-        }
-        $(".slide ul li").fadeOut()
-        $(".slide ul li").eq(i).fadeIn()
-    }
-    setInterval(slide, 3000)
+    // fadeIn, fadeOut 슬라이드
+    $(".slide_list:gt(0)").hide()
+    setInterval(function() {
+        $(".slide_list:first").fadeOut(1500).next().fadeIn(1500)
+        $(".slide_list:first").appendTo(".slide_group")
+    }, 3000)
+    
 
     // 탭 메뉴
     $(".tabmenu>li").click(function() {
-        $("tabmenu>li").removeClass("on")
-        $(this).index()
+        $(".tabmenu>li").removeClass("on")
+        $(this).addClass("on")
 
         let i = $(this).index()
         console.log(i)
@@ -44,11 +43,10 @@ $(function() {
 
     // 메뉴
     $("nav>ul>li").mouseenter(function() {
-        $(".sub").stop().slideDown()
+        $(this).$(".sub").stop().slideDown()
     })
-
     $("nav>ul>li").mouseleave(function() {
-        $(".sub").stop().slideUp()
+        $(this).$(".sub").stop().slideUp()
     })
 
     // 키보드 이벤트 메뉴
@@ -56,11 +54,9 @@ $(function() {
         $(".sub").slideDown()
         $(this).addClass("on")
     })
-
     $(".sub li:last-child").focusout(function() {
         $("nav>ul>li").removeClass("on")
     })
-
     $("nav li:last-child li:last-child").focusout(function() {
         $(".sub").slideUp()
     })
@@ -70,7 +66,6 @@ $(function() {
         $(".popup").show()
         return false
     })
-
     $(".pp").click(function() {
         $(".popup").hide()
     })
